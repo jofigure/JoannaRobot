354f7928ed2b94a8e7387dd338c15cb6cac3dd30
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-10-03 15:33:29 -0400

    gjogjop

--- JoannaRobot/src/chatbot/ChatbotStephanie.java
+++ JoannaRobot/src/chatbot/ChatbotStephanie.java
@@ -1,46 +1,44 @@
 package chatbot;
 
 public class ChatbotStephanie implements Topic {
-
+	
 	private String[] keywords;
-	private String goodbyeKeyword;
-	private String secretKeyword;
+	private String purchaseKeyword;
+	private String nopeKeyword;
 	private String response;
+	private String townhouse;
 	
 	public ChatbotStephanie() {
-		String[] temp = {"townhouse", "town", "row house"};
+		String[] temp = {"townhouse", "town", "community", "row house"};
 		keywords = temp;
-		goodbyeKeyword = "bye";
-		secretKeyword = "pug";
+		purchaseKeyword = "yes";
+		nopeKeyword = "no";
 		response = "";
+		townhouse = "2 family townhome, 27,000 sqft, 6 beds and 4 baths.";
 	}
 
-	@Override
-	public void talk(String  initialInput) {
-		ChatbotMain.print("Hey! So you want to talk about generic boring things, huh? I love talking about that.");
+	
+	
+	public void talk(String initial) {
+		ChatbotMain.print("I know some really nice townhouses, would you be interested in purchasing one?");
 		response = ChatbotMain.getInput();
-		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) != -1) {
-			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("I can't even. I love pugs so much. Wow. You are so cool.");
-				response = ChatbotMain.getInput();
-			}else {
-				ChatbotMain.print("Yeah. That's pretty cool. But there are things I like even more. Tell me something else.");
-				response = ChatbotMain.getInput();
-			}
+		while(ChatbotMain.findKeyword(response, nopeKeyword, 0) == -1) {
+			if(ChatbotMain.findKeyword(response, purchaseKeyword, 0) >= 0)
+				ChatbotMain.print("I am so glad that you're interested, the only townhouse for sale is a " + townhouse + " Does this sound appealing to you?");
+				if(ChatbotMain.findKeyword(response, purchaseKeyword, 0) >= 0)
+				ChatbotMain.print("Thanks for purchasing this townhouse!");
+			else
+				ChatbotMain.print("You are not answering my question.");
+			response = ChatbotMain.getInput();
 		}
-		//access variables from other classes.
-		ChatbotMain.print("Well, it was nice talking to you,"+ChatbotMain.chatbot.getUsername()+"!");
+		ChatbotMain.print("Well, it was nice talking to you, " + ChatbotMain.chatbot.getUsername() + "!");
 		ChatbotMain.chatbot.startChatting();
-		//ChatbotMain.chatbot.getStephanie().talk("");
 	}
 
-	@Override
 	public boolean isTriggered(String response) {
-		for(int i = 0; i < keywords.length; i++) {
-			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0) {
+		for(int i = 0; i < keywords.length; i++)
+			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
 				return true;
-			}
-		}
 		return false;
 	}
 
