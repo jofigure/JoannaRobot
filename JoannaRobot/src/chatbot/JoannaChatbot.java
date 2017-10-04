7e758902666fbdf1f51f3542597012237192c1fb
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-10-04 15:33:16 -0400

    2wsw

--- JoannaRobot/src/chatbot/JoannaChatbot.java
+++ JoannaRobot/src/chatbot/JoannaChatbot.java
@@ -2,7 +2,9 @@ package chatbot;
 
 public class JoannaChatbot implements Topic {
 	
-	private String[] keywords;
+	private String[] keywords1;
+	private String[] keywords2;
+	private String[] keywords3;
 	private String goodbyeKeyword;
 	private String niceKeyword;
 	private String response;
@@ -11,38 +13,65 @@ public class JoannaChatbot implements Topic {
 	public JoannaChatbot() {
 		
 		String[] temp = {"total control","remodel" , "privacy","own", "single family" ,"house"};
-		keywords = temp;
+		keywords1 = temp;
 		goodbyeKeyword = "bye";
 		 niceKeyword = "thanks";
 		response = "";
 		String[] home =  { "Blue 3 floor house" ," Red 1 floor house" , "Wooden house", "Brick no basement house"};
 		houseListings = home;
 		pool = false;
+		String[] temp2 = { "yes","see", "like to","okay"};
+		keywords2 = temp2;
+
 	}
 
 	@Override
 	public void talk(String initialInput) {
-		ChatbotMain.print("Hi, Welcome to JASK real estate! You've been directed to the House Catalogue.");
-		response = ChatbotMain.getInput();
+		ChatbotMain.print("We have great house listings with afforable pricing. Would you like to look for a house?");
+				response = ChatbotMain.getInput();
+		
+	
+	
+	
+		if(ChatbotMain.findKeyword(response,"no" , 0) >=0)
+		{
+			
+		}
+		else
+		{	
+			ChatbotMain.print( "Sorry please input an apporiprate response.");
+
+			response = ChatbotMain.getInput();
+		}
+		
+	
+		
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1)
 		{
 				if(ChatbotMain.findKeyword(response, niceKeyword, 0) >= 0)
 				{
 					
 					ChatbotMain.print( "No problem! Glad to help! Anything else?");
-	
+
 					
 				}
-				ChatbotMain.print("I see you're looking for a house. Does any of these seem of interest to you:" );
+				ChatbotMain.print(" Does any of these seem of interest to you:" );
+				
 				for(int i =0; i< houseListings.length; i++)
 				{
 					int Num = i+1;
-					ChatbotMain.print( Num + houseListings[i]);
+					ChatbotMain.print( Num + ". "  + houseListings[i]);
 					String[] temp = { "Blue","Red", "Wooden", "Brick", "no basement", "1" , "2", "3"  ,"4"};
+					keywords3 = temp;
 					
 				}
+				response = ChatbotMain.getInput();
 			
 		}
+		
+		
+		
+		
 		//access variables from other classes
 		ChatbotMain.print("Well, it was nice talking to you, " + ChatbotMain.chatbot.getUsername()+ "!");
 		ChatbotMain.chatbot.startChatting();
@@ -50,25 +79,91 @@ public class JoannaChatbot implements Topic {
 
 	@Override
 	public boolean isTriggered(String response) {
-		for(int i = 0; i< keywords.length; i++)
+		for(int i = 0; i< keywords1.length; i++)
 		{
-			if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
+			if(ChatbotMain.findKeyword(response, keywords1[i], 0) >= 0)
 			{	
 				return true;
 			}
-			if(ChatbotMain.chatbot.getfamilySize()> 2) 
+			if(ChatbotMain.chatbot.getFamilySize()>= 2 && ChatbotMain.chatbot.getBuying() == true) 
 			{
 				return true;
 				
 			}
-			if()
-			{
-				
-				
-			}
+			
+			
 				
 		}
 		return false;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
