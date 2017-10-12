a937bca7c5edae611103ea7db3e78c2c761649dd
Annie <Annie@ANNIE.home>
2017-10-12 01:05:01 -0400

    .

--- JoannaRobot/src/chatbot/ChatbotAnnie.java
+++ JoannaRobot/src/chatbot/ChatbotAnnie.java
@@ -22,13 +22,18 @@ public class ChatbotAnnie implements Topic {
 		response = buyOrRent = "";
 		String[] temp = {"apt", "apartment", "city", "flat", "condo", "condominium", "co-op", "loft", "duplex"};
 		keywords = temp;
-		String[] temp2 = {"studio apartment", "one bedroom condominium", "three bedroom duplex", "two bedroom loft"};
+		String[] temp2 = {"studio co-op", "studio apartment", "one-bedroom condominium", "three-bedroom duplex", "two-bedroom loft", "two-bedroom co-op"};
 		listings = temp2;
-		String[] temp3 = {"stuff", "stuff2", "stuff3", "stuff4"};
+		String[] temp3 = {"Located in Hell's Kitchen, the apartment features 10' ceilings, 2 closets, a built-in murphy bed, and hardwood floors. This 450 sqft studio co-op is on sale for $419,000.", 
+				"Located in Williamsburg, this spacious alcove studio is conducive for a separate sleeping area from your living/dining space. Featuring white oak hardwood floors, the apartment is up for rent for $2,900 per month.", 
+				"Located in Woodside, the apartment offers two balconies, stainless steel appliances, white marble countertops, a walk-in closet, and hardwood floors. This luxury one-bedroom condominium is on sale for $619,000.", 
+				"Located in the Upper West Side, this massive Brownstone duplex boasts 3 large bedrooms, 2 beautiful bathrooms, a spacious kitchen, a laundry room, as well as a lovely patio. This property is up for rent for $5,900 per month.",
+				"Located in the Flatiron District, this charming loft has 2 bedrooms, 2.5 bathrooms, a washer dryer unit, and oversized windows that offer plenty of light. This 1,281 sqft loft is up for rent for $8,000 per month.",
+				"Located in Forest Hills, the apartment covers 1,050 sqft and features hardwood floors, stainless steel appliances, and plenty of storage space. This listing is on sale for $448,888."};
 		listingsInfo = temp3;
-		boolean[] temp4 = {true, true, false, false};
+		boolean[] temp4 = {true, false, true, false, false, true};
 		onSale = temp4;
-		boolean[] temp5 = {false, false, false, false};
+		boolean[] temp5 = {false, false, false, false, false, false};
 		taken = temp5;
 		isSuggestion = buying = answerAccepted = continueLooking = false;
 		notAnsweringCount = 0;
@@ -41,7 +46,7 @@ public class ChatbotAnnie implements Topic {
 				isSuggestion = false;
 				return true;
 			}
-		if(ChatbotMain.findKeyword(response, "no", 0) >= 0 && ChatbotMain.chatbot.getForLiving() && ChatbotMain.chatbot.getFamilySize() <= 4 && !ChatbotMain.chatbot.getHasPets()) {
+		if(ChatbotMain.findKeyword(response, "no", 0) >= 0 && ChatbotMain.chatbot.getFamilySize() <= 5 && !ChatbotMain.chatbot.getHasPets()) {
 			isSuggestion = true;
 			return true;
 		}
@@ -49,31 +54,27 @@ public class ChatbotAnnie implements Topic {
 	}
 		
 	public void talk(String initial) {
-		answerAccepted = continueLooking = false;
-		notAnsweringCount = 0;
+		setup();
 		if(ChatbotMain.chatbot.getForLiving()) {
 			if(isSuggestion)
-				ChatbotMain.print("It looks like you may be in the market for an apartment. Am I correct?");
+				ChatbotMain.print("It looks like you may be in the market for an apartment.");
 			boolean lookingApt = YesNo("Would you like to take a look at some apartments?");
 			if(lookingApt) {
-				buying = ChatbotMain.chatbot.getBuying();
-				if(buying)
-					buyOrRent = "buy";
-				else
-					buyOrRent = "rent";
 				boolean aptsExist = aptsAvailable();
 				if(aptsExist) {
-					list();
-					ChatbotMain.print("Which one would you like to know more about?");
+					listAsk();
 					while(!answerAccepted && notAnsweringCount <= 2) {
 						response = ChatbotMain.getInput();
 						provideInfo(response);
 					}
 					if(answerAccepted)
-						sell();
-				} else {
-					ChatbotMain.print("Sorry, there are no apartments that you can " + buyOrRent + ".");
-				}
+						sellRent();
+					else {
+						ChatbotMain.print("I guess you don't want to look at properties.");
+						notAnsweringCount++;
+					}
+				} else
+					ChatbotMain.print("Sorry, there are no more apartments that you can " + buyOrRent + ".");
 				if(notAnsweringCount <= 2) {
 					continueLooking = YesNo("Would you like to look at other properties?");
 					if(continueLooking)
@@ -83,48 +84,56 @@ public class ChatbotAnnie implements Topic {
 					ChatbotMain.print("Well, it was nice doing business with you, " + ChatbotMain.chatbot.getUsername() + "!");
 					ChatbotMain.chatbot.startChatting();
 				}
-			}
+			} else
+				ChatbotMain.chatbot.throwBack();	
 		} else
 			mistake();
-		if(notAnsweringCount <= 2)
-			ChatbotMain.chatbot.throwBack();
+	}
+	
+	public void setup() {
+		answerAccepted = continueLooking = false;
+		notAnsweringCount = 0;
+		if(ChatbotMain.chatbot.getForLiving()) {
+			buying = ChatbotMain.chatbot.getBuying();
+			if(buying)
+				buyOrRent = "buy";
+			else
+				buyOrRent = "rent";
+		}
 	}
 
-	private boolean aptsAvailable() {
+	public boolean aptsAvailable() {
 		for(int i = 0; i < listings.length; i++)
 			if(buying == onSale[i] && !taken[i])
 				return true;
 		return false;
 	}
 
-	public void list() {
+	public void listAsk() {
 		ChatbotMain.print("I have some listings that might be of interest to you.");
 		ChatbotMain.print("Properties that you can " + buyOrRent + " include: ");
 		for(int i = 0; i < listings.length; i++)
 			if(buying == onSale[i] && !taken[i])
 				ChatbotMain.print("   - " + listings[i]);
+		ChatbotMain.print("Which one would you like to know more about?");
 	}
 	
 	public void provideInfo(String response) {
-		if(notAnsweringCount < 2) {
-			for(int i = 0; i < listings.length; i++)
-				if(ChatbotMain.findKeyword(response, listings[i], 0) >= 0) {
-					answerAccepted = true;
-					choice = i;
-				}
-			if(answerAccepted)
-				ChatbotMain.print(listingsInfo[choice]);
-			else {
-				ChatbotMain.print("Please pick one of the listings that I mentioned.");
-				notAnsweringCount++;
+		for(int i = 0; i < listings.length; i++)
+			if(ChatbotMain.findKeyword(response, listings[i], 0) >= 0 && buying == onSale[i] && !taken[i]) {
+				answerAccepted = true;
+				choice = i;
 			}
-		} else {
-			ChatbotMain.print("I guess you don't want to look at properties.");
+		if(answerAccepted)
+			ChatbotMain.print(listingsInfo[choice]);
+		else if(notAnsweringCount < 2)
+			ChatbotMain.print("Please pick one of the listings that I mentioned.");
+		if(!answerAccepted && notAnsweringCount <= 2)
 			notAnsweringCount++;
-		}
 	}
 
-	public void sell() {
+	public void sellRent() {
+		ChatbotMain.print("The price is non-negotiable.");
 		boolean buying = YesNo("Would you like to " + buyOrRent + " this unit?");
 		if(buying) {
 			taken[choice] = true;
@@ -135,21 +144,21 @@ public class ChatbotAnnie implements Topic {
 
 	public static int getPositiveNumInput() {
 		ChatbotMain.print("Please enter a number.");
-		String numString = ChatbotMain.getInput();
+		String numStr = ChatbotMain.getInput();
 		boolean isNum = false;
 		int value = 0;
 		while(!isNum)
 			try {
-				value = Integer.parseInt(numString);
+				value = Integer.parseInt(numStr);
 				if(value > 0)
 					isNum = true;
 				else {
 					ChatbotMain.print("You must enter a number greater than 0. Please try again.");
-					numString = ChatbotMain.getInput();
+					numStr = ChatbotMain.getInput();
 				}
 			} catch(NumberFormatException e) {
 				ChatbotMain.print("You must enter a number. Please try again.");
-				numString = ChatbotMain.getInput();
+				numStr = ChatbotMain.getInput();
 			}
 		return value;
 	}
