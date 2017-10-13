260163936b085930c66995f88ba38f37cf2ee873
joann <joann@DESKTOP-56VO8EV>
2017-10-12 22:12:41 -0400

    wwwwwwwwww

--- JoannaRobot/src/chatbot/JoannaChatbot.java
+++ JoannaRobot/src/chatbot/JoannaChatbot.java
@@ -22,7 +22,7 @@ public class JoannaChatbot implements Topic {
 		goodbyeKeyword = "bye";
 		 niceKeyword = "thanks";
 		response = "";
-		String[] home =  { " A blue 3 floor house" ," A red 1 floor house" , "A wooden house", "A brick no basement house"};
+		String[] home =  { "A blue 3 floor house" ,"A red 1 floor house" , "A wooden house", "A brick no basement house"};
 		houseListings = home;
 		pool = false;
 		String[] adj = { "Blue","Red", "Wooden", "Brick", "no basement", "1" , "2", "3"  ,"4"};
@@ -34,51 +34,40 @@ public class JoannaChatbot implements Topic {
 	public void talk(String initialInput) {
 		
 		if(ChatbotMain.chatbot.getForLiving()) {
-		  
-			
-			
 
 			ChatbotMain.print("We have great house listings with affordable pricing.");
 					
-					
-					
-					 wantHouse=ChatbotAnnie.YesNo("Would you like to look at the houses?");
+			wantHouse=ChatbotAnnie.YesNo("Would you like to look at the houses?");
 			while(!wantHouse)
 			{
-						getPleads();
-							if(pleading[keepPlead].equals(pleading[2]))
-							{	if(wantPool)
-								{
-								pool= true;
-								
+				getPleads();
+					if(pleading[keepPlead].equals(pleading[2]))
+					{	if(wantPool)
+						{
+						pool= true;
+						
+						listOptions();
+						break;
+						}
+					else {
+						if(ChatbotAnnie.YesNo("Last chance... Want a pool?"))
+							{
+							
+						pool= true;
+							
 								listOptions();
 								break;
-								}
-							else {
-								if(ChatbotAnnie.YesNo("Last chance... Want a pool?"))
-									{
-									
-								pool= true;
-									
-										listOptions();
-										break;
-									}
 							}
-								ChatbotMain.print( "Judging from your responses,buying a house isn't for you.");
-							ChatbotMain.chatbot.throwBack();
-								
-							}
-							
-							
-						}
-				
+					}
+						ChatbotMain.print( "Judging from your responses, buying a house isn't for you.");
+					ChatbotMain.chatbot.throwBack();
+						
+					}
+					
+					
+				}
+		
 				listOptions();
-			
-			
-			
-			
-			
-			
 		} else
 			ChatbotAnnie.mistake();
 		} 
@@ -86,48 +75,44 @@ public class JoannaChatbot implements Topic {
 	
 
 	private void listOptions() {
-		
-		ChatbotMain.print(" Do any of these seem of interest to you:" );
-		
+				
 		for(int i =0; i< houseListings.length; i++)
 		{
 			int Num = i+1;
 			ChatbotMain.print( Num + ". "  + houseListings[i]);
 			
+		}
+		boolean likeHouse = ChatbotAnnie.YesNo("Do any of these seem of interest to you?");
+		if(likeHouse)
+		{
+			ChatbotMain.print("Okay which one would you like to buy? Please mention the color/number/material of the house. ");
+			buyhouse();
+		} else { 
+			
+			ChatbotMain.print("Now you're just playing around. We are a very busy real estate company. Please come again when you have decided what you want to buy or rent. See you! "); 
+			keepPlead=-1;
+			ChatbotMain.chatbot.startChatting();
 			
 		}
-		boolean likeHouse = ChatbotAnnie.YesNo(" ?");
-		if(likeHouse)
-				{
-				ChatbotMain.print(" Okay which one would you like to buy? Please mention the color/number/material of the house. ");
-					buyhouse();
-				} else { 
-					
-					ChatbotMain.print("Now you're just playing around. We are a very busy real estate company. Please come again when you have decided what you want to buy or rent. See you! "); 
-					keepPlead=-1;
-					ChatbotMain.chatbot.startChatting();
-					
-				}
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1)
 		{
-				if(ChatbotMain.findKeyword(response, niceKeyword, 0) >= 0)
-				{
-					
-					ChatbotMain.print( "No problem! Glad to help! Bye!");
-
-					
-				}
+			if(ChatbotMain.findKeyword(response, niceKeyword, 0) >= 0)
+			{
 				
-				response = ChatbotMain.getInput();
+				ChatbotMain.print( "No problem! Glad to help! Bye!");
+
+				
+			}
+			
+			response = ChatbotMain.getInput();
 		}
 	
-		ChatbotMain.print("	It was nice talking to you, " + ChatbotMain.chatbot.getUsername()+ "!");
+		ChatbotMain.print("It was nice talking to you, " + ChatbotMain.chatbot.getUsername()+ "!");
 		ChatbotMain.chatbot.startChatting();
 	}
 
-	@Override
 	public boolean isTriggered(String response) {
-		String[] pleads = {"Do you like houses?", "Houses are great for " + convertFam() + " people, right?" , "If you have a house,you can build your own personal pool in the backyard. Would you like one?"};
+		String[] pleads = {"Do you like houses?", "Houses are great for " + convertFam() + " people, right?" , "If you have a house, you can build your own personal pool in the backyard. Would you like one?"};
 		pleading = pleads;
 		for(int i = 0; i< keywords1.length; i++)
 		{
@@ -135,7 +120,7 @@ public class JoannaChatbot implements Topic {
 			{	
 				return true;
 			}
-			if(ChatbotMain.chatbot.getFamilySize()>= 2 && ChatbotMain.chatbot.getBuying() == true && ChatbotMain.findKeyword(response, "no", 0)>= 0) 
+			if((ChatbotMain.chatbot.getFamilySize()>= 2 || ChatbotMain.chatbot.getBuying() == true) && ChatbotMain.findKeyword(response, "no", 0)>= 0) 
 			{
 				return true;
 				
@@ -156,133 +141,50 @@ public class JoannaChatbot implements Topic {
 			wantHouse=ChatbotAnnie.YesNo(pleading[keepPlead]);
 		}
 		else {
-		if(keepPlead==2)
+			if(keepPlead==2)
 			{
 				wantPool=ChatbotAnnie.YesNo(pleading[keepPlead]);
 			}
-	else {
-			ChatbotMain.print("Now you're just playing around. We are a very busy real estate company. Please come again when you have decided what you want to buy or rent. See you! "); 
-			keepPlead=-1;
-			ChatbotMain.chatbot.startChatting();
-		}
+			else {
+				ChatbotMain.print("Now you're just playing around. We are a very busy real estate company. Please come again when you have decided what you want to buy or rent. See you! "); 
+				keepPlead=-1;
+				ChatbotMain.chatbot.startChatting();
+			}
 		}
 	}
  
 
-public String convertFam()
-{
-
+	public String convertFam()
+	{	
+		String str =String.valueOf(ChatbotMain.chatbot.getFamilySize());  
+		return str;
+	}	
 	
- String str =String.valueOf(ChatbotMain.chatbot.getFamilySize());  
-	return str;
-}
-
-
-
-
-public boolean buyhouse()
-{	
 	
-	response = ChatbotMain.getInput();
-	for(int i = 0; i< keywords3.length; i++)
-	{
-		if(ChatbotMain.findKeyword(response, keywords3[i], 0) >= 0)
-		{	
-			ChatbotMain.print("You've successfully bought the " + keywords3[i]+ " house! Congrats. ");
-			if(pool==true)
-			{
-			ChatbotMain.print("And with the pool expansion too!Hope you enjoy your new place.");
-			}
-			ChatbotMain.print("Now you can either say thanks or say bye, if you don't I won't respond. ");
-			response = ChatbotMain.getInput(); 
-			if(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) >= 0)
-			{
-				ChatbotMain.print("Aren't you a grumpy one! I, on the other hand am not so ");
-			}
-		} 
+	public boolean buyhouse()
+	{	
 	
+		response = ChatbotMain.getInput();
+		for(int i = 0; i< keywords3.length; i++)
+		{
+			if(ChatbotMain.findKeyword(response, keywords3[i], 0) >= 0)
+			{	
+				ChatbotMain.print("You've successfully bought the " + keywords3[i]+ " house! Congrats. ");
+				if(pool==true)
+				{
+					ChatbotMain.print("And with the pool expansion too! Hope you enjoy your new place.");
+				}
+				ChatbotMain.print("Now you can either say thanks or say bye; if you don't, I won't respond.");
+				response = ChatbotMain.getInput(); 
+				if(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) >= 0)
+				{
+					ChatbotMain.print("Aren't you a grumpy one! I, on the other hand am not.");
+				}
+			} 
 		
-	}
-	return false;
-}
-
-
-public boolean getYesNo(String reponse)
-{
-	
-	this.response= reponse;  
-	if( response.equals("yes")==true || response.equals("no")==true)
-	{
-		ChatbotMain.print("Would you like to look for a house?Please answer with yes or no");
-		return true;
-		
-		
-	}
-	else {
-		
+			
+		}
 		return false;
-		
 	}
-}
-
-
- 
-
-
-
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
